ff3b19279c15b6a31bce004437687e689e2df95b
Ian Lewis <ijl20@cam.ac.uk>
2016-04-21 20:01:39 +0100

    feedhandler updated to load parameters from config(), can start as service e.g. "service:uk.ac.cam.tfc_server.feedhandler.A"

--- src/main/java/uk/ac/cam/tfc_server/feedhandler/FeedHandler.java
+++ src/main/java/uk/ac/cam/tfc_server/feedhandler/FeedHandler.java
@@ -4,7 +4,7 @@ package uk.ac.cam.tfc_server.feedhandler;
 // *************************************************************************************************
 // *************************************************************************************************
 // FeedHandler.java
-// Version 0.06
+// Version 0.07
 // Author: Ian Lewis ijl20@cam.ac.uk
 //
 // Forms part of the 'tfc_server' next-generation Realtime Intelligent Traffic Analysis system
@@ -36,7 +36,6 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.file.FileSystem;
 import io.vertx.core.eventbus.EventBus;
-//import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 
@@ -53,71 +52,57 @@ import com.google.transit.realtime.GtfsRealtime.VehicleDescriptor;
 import com.google.transit.realtime.GtfsRealtime.TripDescriptor;
 import com.google.transit.realtime.GtfsRealtime.Position;
 
-//import uk.ac.cam.tfc_server.gtfs.PositionRecord;
-
 public class FeedHandler extends AbstractVerticle {
 
-  private HttpServer http_server = null;
-  private final int HTTP_PORT = 8080;
+    // from config()
+    private String MODULE_NAME;       // config module.name - normally "feedhandler"
+    private String MODULE_ID;         // config module.id
+    private String EB_SYSTEM_STATUS;  // config eb.system_status
+    private String EB_MANAGER;        // config eb.manager
+    
+    private int HTTP_PORT;            // config feedplayer.http.port
 
-  private String tfc_data_cache = null; // environment var -- store backup bin files
-  private String tfc_data_bin = null; // environment var -- store bin gtfs files
-  private String tfc_data_monitor = null; // environment var -- store latest bin file
+    private String FEEDHANDLER_ADDRESS; // config MODULE_NAME.address
+    
+    private String TFC_DATA_CACHE = null;   // MODULE_NAME.tfc_data_cache
+    private String TFC_DATA_BIN = null;     // MODULE_NAME.tfc_data_bin
+    private String TFC_DATA_MONITOR = null; // MODULE_NAME.tfc_data_monitor
 
-  private EventBus eb = null;
+    private final int SYSTEM_STATUS_PERIOD = 10000; // publish status heartbeat every 10 s
+    private final int SYSTEM_STATUS_AMBER_SECONDS = 15;
+    private final int SYSTEM_STATUS_RED_SECONDS = 25;
+    
+    private HttpServer http_server = null;
+    private EventBus eb = null;
     
   @Override
   public void start(Future<Void> fut) throws Exception {
 
     boolean ok = true; // simple boolean to flag an abort during startup
 
-    System.out.println("FeedHandler started!");
+    // load Zone initialization values from config()
+    if (!get_config())
+          {
+              System.err.println("FeedHandler: "+ MODULE_ID + " failed to load initial config()");
+              vertx.close();
+              return;
+          }
+      
+    System.out.println("FeedHandler: " + MODULE_ID + " started, sending to "+FEEDHANDLER_ADDRESS);
 
     eb = vertx.eventBus();
     
-    tfc_data_cache = System.getenv("TFC_DATA_CACHE");
-    if (tfc_data_cache == null)
-    {
-      System.err.println("TFC_DATA_CACHE environment var not set -- aborting feedhandler startup");
-      ok = false;
-      vertx.close();
-    }
+    http_server = vertx.createHttpServer();
 
-    if (ok)
-    {
-      tfc_data_bin = System.getenv("TFC_DATA_BIN");
-      if (tfc_data_bin == null)
-      {
-        System.err.println("TFC_DATA_BIN environment var not set -- aborting feedhandler startup");
-        ok = false;
-        vertx.close();
-      }
-    }
-
-    if (ok)
-    {
-      tfc_data_monitor = System.getenv("TFC_DATA_MONITOR");
-      if (tfc_data_monitor == null)
-      {
-        System.err.println("TFC_DATA_MONITOR environment var not set -- aborting feedhandler startup");
-        ok = false;
-        vertx.close();
-      }
-    }
-
-    if (ok)
-    {
-      http_server = vertx.createHttpServer();
-
-      http_server.requestHandler(new Handler<HttpServerRequest>() {
+    http_server.requestHandler(new Handler<HttpServerRequest>() {
         @Override
         public void handle(HttpServerRequest request) {
-            System.out.println("FeedHandler called handle!");
+            //System.out.println("FeedHandler called handle!");
             if(request.method() == HttpMethod.POST) {
 
               request.bodyHandler(body_data -> {
-                System.out.println("FeedHandler called bodyHandler!");
-                System.out.println("Full body received, length(" + body_data.length()+")");
+                      //System.out.println("FeedHandler called bodyHandler!");
+                      //System.out.println("Full body received, length(" + body_data.length()+")");
 
                 try {
                   process_gtfs(body_data);
@@ -145,9 +130,81 @@ public class FeedHandler extends AbstractVerticle {
           fut.fail(result.cause());
         }
       });
-    } // end if (ok)
+
+    // send periodic "system_status" messages
+    vertx.setPeriodic(SYSTEM_STATUS_PERIOD, id -> {
+      eb.publish(EB_SYSTEM_STATUS,
+                 "{ \"module_name\": \""+MODULE_NAME+"\"," +
+                   "\"module_id\": \""+MODULE_ID+"\"," +
+                   "\"status\": \"UP\"," +
+                   "\"status_amber_seconds\": "+String.valueOf( SYSTEM_STATUS_AMBER_SECONDS ) + "," +
+                   "\"status_red_seconds\": "+String.valueOf( SYSTEM_STATUS_RED_SECONDS ) +
+                 "}" );
+      });
+
   } // end start()
 
+    // Load initialization global constants defining this Zone from config()
+    private boolean get_config()
+    {
+        // config() values needed by all TFC modules are:
+        //   module.name - usually "zone"
+        //   module.id - unique module reference to be used by this verticle
+        //   eb.system_status - String eventbus address for system status messages
+        //   eb.manager - eventbus address for manager messages
+        
+        MODULE_NAME = config().getString("module.name");
+        if (MODULE_NAME == null)
+            {
+                System.err.println("FeedHandler: config() not set");
+                return false;
+            }
+        
+        MODULE_ID = config().getString("module.id");
+        if (MODULE_ID==null) return false;
+
+        EB_SYSTEM_STATUS = config().getString("eb.system_status");
+
+        EB_MANAGER = config().getString("eb.manager");
+
+        FEEDHANDLER_ADDRESS = config().getString(MODULE_NAME+".address");
+        if (FEEDHANDLER_ADDRESS == null)
+            {
+                System.err.println("FeedHandler: "+MODULE_ID+" "+MODULE_NAME+".address config() not set");
+                return false;
+            }
+
+        HTTP_PORT = config().getInteger(MODULE_NAME+".http.port",0);
+        if (HTTP_PORT == 0)
+        {
+          System.err.println("tfc_data_cache config() var not set -- aborting feedhandler startup");
+          return false;
+        }
+
+        TFC_DATA_CACHE = config().getString(MODULE_NAME+".tfc_data_cache");
+        if (TFC_DATA_CACHE == null)
+        {
+          System.err.println("tfc_data_cache config() var not set -- aborting feedhandler startup");
+          return false;
+        }
+
+        TFC_DATA_BIN = config().getString(MODULE_NAME+".tfc_data_bin");
+        if (TFC_DATA_BIN == null)
+        {
+          System.err.println("tfc_data_bin config() var not set -- aborting feedhandler startup");
+          return false;
+        }
+
+        TFC_DATA_MONITOR = config().getString(MODULE_NAME+".tfc_data_monitor");
+        if (TFC_DATA_MONITOR == null)
+        {
+          System.err.println("tfc_data_monitor config() var not set -- aborting feedhandler startup");
+          return false;
+        }
+        
+        return true;
+    }
+    
     // process the POST gtfs binary data
     private void process_gtfs(Buffer buf) throws Exception {
 
@@ -170,7 +227,7 @@ public class FeedHandler extends AbstractVerticle {
     //
     // if full directory path exists, then write file
     // otherwise create full path first
-    final String bin_path = tfc_data_bin+"/"+filepath;
+    final String bin_path = TFC_DATA_BIN+"/"+filepath;
     System.out.println("Writing "+bin_path+"/"+filename+".bin");
     fs.exists(bin_path, result -> {
             if (result.succeeded() && result.result())
@@ -196,7 +253,7 @@ public class FeedHandler extends AbstractVerticle {
 
     // Write file to $TFC_DATA_CACHE
     //
-    final String cache_path = tfc_data_cache+"/"+filepath;
+    final String cache_path = TFC_DATA_CACHE+"/"+filepath;
     System.out.println("Writing "+cache_path+"/"+filename+".bin");
     // if full directory path exists, then write file
     // otherwise create full path first
@@ -224,8 +281,8 @@ public class FeedHandler extends AbstractVerticle {
 
     // Write file to $TFC_DATA_MONITOR
     //
-    System.out.println("Writing "+tfc_data_monitor+"/"+filename+".bin");
-    fs.readDir(tfc_data_monitor, ".*\\.bin", monitor_result -> {
+    System.out.println("Writing "+TFC_DATA_MONITOR+"/"+filename+".bin");
+    fs.readDir(TFC_DATA_MONITOR, ".*\\.bin", monitor_result -> {
                             if (monitor_result.succeeded())
                                 {
                                     for (String f: monitor_result.result())
@@ -238,21 +295,19 @@ public class FeedHandler extends AbstractVerticle {
                                                         }
                                                 });
                                         }
-                                    write_file(fs, buf, tfc_data_monitor+"/"+filename+".bin");
+                                    write_file(fs, buf, TFC_DATA_MONITOR+"/"+filename+".bin");
                                 }
                             else
                                 {
-                                    System.err.println("FeedHandler error reading tfc_data_monitor path: "+tfc_data_monitor);
+                                    System.err.println("FeedHandler error reading tfc_data_monitor path: "+TFC_DATA_MONITOR);
                                     System.err.println(monitor_result.cause());
                                 }
     });
 
     // Here is where we process the individual position records
     FeedMessage feed = FeedMessage.parseFrom(buf.getBytes());
-    //EventBus eb = vertx.eventBus();
-    //eb.publish("feed_vehicle", Json.encode(feed));
     
-    eb.publish("feed_vehicle", feed_to_json_object(feed,filename,filepath));
+    eb.publish(FEEDHANDLER_ADDRESS, feed_to_json_object(feed,filename,filepath));
     System.out.println("FeedHandler published (feed_vehicle, pos_records)");
     
   } // end process_gtfs()
