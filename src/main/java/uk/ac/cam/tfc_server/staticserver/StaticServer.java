bd1e5dee3a53c7e0ce059155d7b8b3fe51d70c0a
Ian Lewis <ijl20@cam.ac.uk>
2016-07-02 13:07:39 +0100

    StaticServer.java created. cluster.xml modified to disable multicast and use tcp/ip on localhost only

---
+++ src/main/java/uk/ac/cam/tfc_server/staticserver/StaticServer.java
@@ -0,0 +1,194 @@
+package uk.ac.cam.tfc_server.staticserver;
+
+// *************************************************************************************************
+// *************************************************************************************************
+// *************************************************************************************************
+// StaticServer.java
+//
+// StaticServer is a basic http server for service static data from webroot/static
+//
+// Author: Ian Lewis ijl20@cam.ac.uk
+//
+// Forms part of the 'tfc_server' next-generation Realtime Intelligent Traffic Analysis system
+//
+// *************************************************************************************************
+// *************************************************************************************************
+// *************************************************************************************************
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.DeploymentOptions;
+
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.http.HttpMethod;
+
+import io.vertx.core.eventbus.EventBus;
+
+// vertx web, service proxy, sockjs eventbus bridge
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class StaticServer extends AbstractVerticle {
+
+    private Integer HTTP_PORT; // from config()
+
+    private String EB_SYSTEM_STATUS; // from config()
+    private String EB_MANAGER; // from config()
+    private String MODULE_NAME; // from config()
+    private String MODULE_ID; // from config()
+    private String WEBROOT; // from config()
+    
+    private String BASE_URI; // used as template parameter for web pages, built from config()
+    
+    private final int SYSTEM_STATUS_PERIOD = 10000; // publish status heartbeat every 10 s
+    private final int SYSTEM_STATUS_AMBER_SECONDS = 15;
+    private final int SYSTEM_STATUS_RED_SECONDS = 25;
+
+    // Vertx event bus
+    private EventBus eb = null;
+
+    @Override
+    public void start(Future<Void> fut) throws Exception
+    {
+
+    // Get src/main/conf/tfc_server.conf config values for module
+    if (!get_config())
+        {
+            System.err.println("StaticServer: problem loading config");
+            vertx.close();
+            return;
+        }
+
+    System.out.println("StaticServer starting as "+MODULE_NAME+"."+MODULE_ID+
+                       " on port "+HTTP_PORT );
+
+    BASE_URI = MODULE_NAME; // typically 'staticserver'
+
+    eb = vertx.eventBus();
+
+    // send periodic "system_status" messages
+    init_system_status();
+    
+    // *************************************************************************************
+    // *************************************************************************************
+    // *********** Start StaticServer web server                                ************
+    // *************************************************************************************
+    // *************************************************************************************
+    HttpServer http_server = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+
+
+    // ********************************
+    // create handler for embedded page
+    // ********************************
+
+    router.route("/static/"+BASE_URI+"/home").handler( routingContext -> {
+
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("content-type", "text/html");
+
+        response.end("<h1>Rita::StaticServer."+MODULE_ID+"</h1><p>Vertx-Web!</p>");
+    });
+
+
+    // ********************************
+    // create handler for static pages
+    // ********************************
+
+    StaticHandler static_handler = StaticHandler.create();
+    static_handler.setWebRoot(WEBROOT);
+    static_handler.setCachingEnabled(false);
+    router.route(HttpMethod.GET, "/static/*").handler( static_handler );
+
+    System.out.println("StaticHandler."+MODULE_ID+" using "+WEBROOT);
+    
+    // ********************************
+    // connect router to http_server
+    // ********************************
+
+    http_server.requestHandler(router::accept).listen(HTTP_PORT);
+
+  } // end start()
+
+    // Set periodic timer to broadcast "system UP" status messages to EB_SYSTEM_STATUS address
+    private void init_system_status()
+    {
+    vertx.setPeriodic(SYSTEM_STATUS_PERIOD, id -> {
+      eb.publish(EB_SYSTEM_STATUS,
+                 "{ \"module_name\": \""+MODULE_NAME+"\"," +
+                   "\"module_id\": \""+MODULE_ID+"\"," +
+                   "\"status\": \"UP\"," +
+                   "\"status_amber_seconds\": "+String.valueOf( SYSTEM_STATUS_AMBER_SECONDS ) + "," +
+                   "\"status_red_seconds\": "+String.valueOf( SYSTEM_STATUS_RED_SECONDS ) +
+                 "}" );
+      });
+    }
+    
+    // Load initialization global constants defining this module from config()
+    private boolean get_config()
+    {
+        // config() values needed by all TFC modules are:
+        // module.name e.g. "rita"
+        // module.id e.g. "A"
+        // eb.system_status - String eventbus address for system status messages
+        // eb.manager - evenbus address to subscribe to for system management messages
+
+        MODULE_NAME = config().getString("module.name"); // "staticserver"
+        if (MODULE_NAME==null)
+            {
+                System.err.println("StaticServer: no module.name in config()");
+                return false;
+            }
+        
+        MODULE_ID = config().getString("module.id"); // A, B, ...
+        if (MODULE_ID==null)
+            {
+                System.err.println("StaticServer: no module.id in config()");
+                return false;
+            }
+
+        // common system status reporting address, e.g. for UP messages
+        // picked up by Console
+        EB_SYSTEM_STATUS = config().getString("eb.system_status");
+        if (EB_SYSTEM_STATUS==null)
+            {
+                System.err.println("StaticServer: no eb.system_status in config()");
+                return false;
+            }
+
+        // system control address - commands are broadcast on this
+        EB_MANAGER = config().getString("eb.manager");
+        if (EB_MANAGER==null)
+            {
+                System.err.println("StaticServer: no eb.manager in config()");
+                return false;
+            }
+
+        // port for user browser access to this Rita
+        HTTP_PORT = config().getInteger(MODULE_NAME+".http.port");
+        if (HTTP_PORT==null)
+            {
+                System.err.println("StaticServer: no "+MODULE_NAME+".http.port in config()");
+                return false;
+            }
+
+        // where the built-in webserver will find static files
+        WEBROOT = config().getString(MODULE_NAME+".webroot");
+        if (WEBROOT==null)
+            {
+                System.err.println("StaticServer: no "+MODULE_NAME+".webroot in config()");
+                return false;
+            }
+
+        return true;
+    }
+
+} // end class Rita
+
+
