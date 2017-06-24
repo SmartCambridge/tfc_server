382b3ca913f796d1ec1c20cd64a46bb9138e48af
Ian Lewis <ijl20@cam.ac.uk>
2017-06-24 16:42:14 +0100

    MsgRouter - initial functional version, POSTing data onwards

--- src/main/java/uk/ac/cam/tfc_server/msgrouter/MsgRouter.java
+++ src/main/java/uk/ac/cam/tfc_server/msgrouter/MsgRouter.java
@@ -18,8 +18,14 @@ import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpClientRequest;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 // other tfc_server classes
 import uk.ac.cam.tfc_server.util.Log;
@@ -27,7 +33,7 @@ import uk.ac.cam.tfc_server.util.Constants;
 
 public class MsgRouter extends AbstractVerticle {
 
-    private final String VERSION = "0.02";
+    private final String VERSION = "0.03";
     
     // from config()
     public int LOG_LEVEL;             // optional in config(), defaults to Constants.LOG_INFO
@@ -36,8 +42,6 @@ public class MsgRouter extends AbstractVerticle {
     private String EB_SYSTEM_STATUS;  // config eb.system_status
     private String EB_MANAGER;        // config eb.manager
     
-    private ArrayList<RouterConfig> START_ROUTERS; // config msgrouters.routers parameters
-    
     private final int SYSTEM_STATUS_PERIOD = 10000; // publish status heartbeat every 10 s
     private final int SYSTEM_STATUS_AMBER_SECONDS = 25;
     private final int SYSTEM_STATUS_RED_SECONDS = 35;
@@ -45,6 +49,11 @@ public class MsgRouter extends AbstractVerticle {
     private EventBus eb = null;
     private Log logger;
         
+    private ArrayList<JsonObject> START_ROUTERS; // config msgrouters.routers parameters
+
+    // global vars
+    private HashMap<String,HttpClient> http_clients; // used to store a HttpClient for each feed_id
+    
   @Override
   public void start(Future<Void> fut) throws Exception {
       
@@ -56,13 +65,14 @@ public class MsgRouter extends AbstractVerticle {
               return;
           }
 
-    System.out.println("debug: LOG_LEVEL "+LOG_LEVEL);
     logger = new Log(LOG_LEVEL);
     
-    logger.log(Constants.LOG_INFO, MODULE_NAME+"."+MODULE_ID+": Version "+VERSION+" started");
-
+    logger.log(Constants.LOG_INFO, MODULE_NAME+"."+MODULE_ID+": Version "+VERSION+" started with log_level "+LOG_LEVEL);
 
     eb = vertx.eventBus();
+
+    // create holder for HttpClients, one per router
+    http_clients = new HashMap<String,HttpClient>();
 
     // iterate through all the routers to be started
     for (int i=0; i<START_ROUTERS.size(); i++)
@@ -95,48 +105,108 @@ public class MsgRouter extends AbstractVerticle {
     // start_router()
     // start a Router by registering a consumer to the given address
     // ************************************************************
-    private void start_router(RouterConfig router_config)
+    private void start_router(JsonObject router_config)
     {
-        String router_filter;
-        if (router_config.source_filter == null)
+        JsonObject filter_json = router_config.getJsonObject("source_filter");
+        boolean has_filter =  filter_json != null;
+
+        final RouterFilter source_filter = has_filter ? new RouterFilter(filter_json) : null;
+
+        final HttpClient http_client = vertx.createHttpClient( new HttpClientOptions()
+                                                       .setSsl(router_config.getBoolean("http.ssl"))
+                                                       .setTrustAll(true)
+                                                       .setDefaultPort(router_config.getInteger("http.port"))
+                                                       .setDefaultHost(router_config.getString("http.host"))
+                                                             );
+        String router_filter_text;
+        if (has_filter)
             {
-                router_filter = "";
+                //source_filter = new RouterFilter(filter_json);  
+                router_filter_text = " with " + filter_json.toString();
             }
         else
             {
-                router_filter = " with " + router_config.source_filter.toString();
+                router_filter_text = "";
             }
-        System.out.println("MsgRouter."+MODULE_ID+": starting router "+router_config.source_address+ router_filter);
+        logger.log(Constants.LOG_INFO, MODULE_NAME+"."+MODULE_ID+
+                   ": starting router "+router_config.getString("source_address")+ router_filter_text);
 
-        RouterUtils router_utils = new RouterUtils(vertx, router_config);
+        //RouterUtils router_utils = new RouterUtils(vertx, router_config);
         
         // register to router_config.source_address,
         // test messages with router_config.source_filter
         // and call store_msg if current message passes filter
-        eb.consumer(router_config.source_address, message -> {
+        eb.consumer(router_config.getString("source_address"), message -> {
             //System.out.println("MsgRouter."+MODULE_ID+": got message from " + router_config.source_address);
             JsonObject msg = new JsonObject(message.body().toString());
             
             //System.out.println(msg.toString());
 
-            if (router_config.source_filter != null && router_config.source_filter.match(msg))
+            if (!has_filter || source_filter.match(msg))
             {
-                // store this message if it matches the filter within the RouterConfig
-                router_utils.route_msg(msg);
+                // route this message if it matches the filter within the RouterConfig
+                route_msg(http_client, router_config, msg);
                 return;
             }
             else
             {
-                System.out.println("debug: skipping"+LOG_LEVEL);
                 logger.log(Constants.LOG_DEBUG, MODULE_NAME+"."+MODULE_ID+
-                     ": msg skipped no match "+router_config.source_filter.toString());
+                     ": msg skipped no match "+router_config.getJsonObject("source_filter").toString());
             }
 
         });
     
     } // end start_router
 
-    
+    //**************************************************************************
+    //**************************************************************************
+    // Route the message onwards via POST to destination in config
+    //**************************************************************************
+    //**************************************************************************
+    private void route_msg(HttpClient http_client, JsonObject router_config, JsonObject msg)
+    {
+        logger.log(Constants.LOG_DEBUG, MODULE_NAME+"."+MODULE_ID+
+                   ": routing " + msg);
+
+        // Sample router_config
+        //        { 
+        //            "source_address": "tfc.everynet_feed.test",
+        //            "source_filter": { 
+        //                                 "field": "dev_eui",
+        //                                 "compare": "=",
+        //                                 "value": "0018b2000000113e"
+        //                             },
+        //             "http.host":  "localhost",              
+        //             "http.uri" :  "/everynet_feed/test/adeunis_test2",
+        //             "http.ssl":   false,
+        //             "http.port":  8098,
+        //             "http.post":  true,
+        //             "http.token": "cam-auth-test"
+        //        }
+
+        String http_uri = router_config.getString("http.uri");
+
+        HttpClientRequest request = http_client.post(http_uri, response -> {
+                logger.log(Constants.LOG_DEBUG, MODULE_NAME+"."+MODULE_ID+
+                     ": msg posted to " + http_uri);
+
+                logger.log(Constants.LOG_DEBUG, MODULE_NAME+"."+MODULE_ID+
+                     ": response was " + response.statusCode());
+
+            });
+        // Now do stuff with the request
+        request.putHeader("content-type", "application/json");
+        request.timeout(15000);
+
+        String auth_token = router_config.getString("http.token");
+        if (auth_token != null)
+        {
+            request.putHeader("X-Auth-Token", auth_token);
+        }
+        // Make sure the request is ended when you're done with it
+        request.end(msg.getJsonArray("request_data").getJsonObject(0).toString());
+    }
+
     //**************************************************************************
     //**************************************************************************
     // Load initialization global constants defining this MsgRouter from config()
@@ -165,7 +235,6 @@ public class MsgRouter extends AbstractVerticle {
         }
 
         LOG_LEVEL = config().getInteger(MODULE_NAME+".log_level", 0);
-        System.out.println("debug: config LOG_LEVEL "+LOG_LEVEL);
         if (LOG_LEVEL==0)
             {
                 LOG_LEVEL = Constants.LOG_INFO;
@@ -186,7 +255,7 @@ public class MsgRouter extends AbstractVerticle {
         }
 
         // iterate through the msgrouter.routers config values
-        START_ROUTERS = new ArrayList<RouterConfig>();
+        START_ROUTERS = new ArrayList<JsonObject>();
         JsonArray config_router_list = config().getJsonArray(MODULE_NAME+".routers");
         for (int i=0; i<config_router_list.size(); i++)
             {
@@ -196,9 +265,9 @@ public class MsgRouter extends AbstractVerticle {
                 config_json.put("module_name", MODULE_NAME);
                 config_json.put("module_id", MODULE_ID);
                 
-                RouterConfig router_config = new RouterConfig(config_json);
+                //RouterConfig router_config = new RouterConfig(config_json);
                 
-                START_ROUTERS.add(router_config);
+                START_ROUTERS.add(config_json);
             }
 
         return true;
