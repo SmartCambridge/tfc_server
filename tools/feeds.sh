d718a75e5555d3088c58000b55c130a4c907d3e9
Ian Lewis <ijl20@cam.ac.uk>
2020-06-12 17:58:58 +0100

    itoworld feedmaker.B, msgrouter, msgfiler ok

---
+++ tools/feeds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TODAY=$(date +'%Y/%m/%d')
+
+FEEDS=(
+/media/tfc/sirivm_json/data_monitor
+/media/tfc/itoworld/sirivm/data_monitor
+/media/tfc/itoworld/sirivm/data_monitor_json
+/media/tfc/cam_park_rss/data_monitor
+/media/tfc/cam_park_rss/data_monitor_json
+/media/tfc/csn_ttn/data_monitor
+/media/tfc/google_traffic_map/cambridge/$TODAY
+/media/tfc/btjourney/journeytimes/data_monitor
+/media/tfc/btjourney/journeytimes/data_monitor_json
+)
+
+date
+for f in ${FEEDS[@]}
+do
+    ts=$(stat --printf='%Z' $f)
+    printf "%12s %s\n" "$(date -d @$ts)" $f
+done
+
+
